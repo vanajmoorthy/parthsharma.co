ce1f31f93ca3353bc8f15475061d7a5710725c28
vanajmoorthy <vanajmoorthy@gmail.com>
2020-03-28 18:03:56 +0530

    all

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	theme: {
+		fontFamily: {
+			hbold: ["HelveticaBold", "sans-serif"],
+      hmed: ["HelveticaLight", "sans-serif"],
+			hlight: ["HelveticaMedium", "sans-serif"]
+		},
+		extend: {}
+	},
+	variants: {},
+	plugins: []
+};
